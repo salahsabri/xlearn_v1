2343976753f2cb4cf784577e91696f1bdd600a56
salahsabri <41323692+salahsabri@users.noreply.github.com>
2018-11-26 16:51:22 +1100

    Update segmentation.py

--- doc/demo/segmentation.py
+++ doc/demo/segmentation.py
@@ -3,7 +3,7 @@ from skimage import io
 import imageio
 #import dxchange
 
-with open(paths.txt) as f:
+with open('paths.txt') as f:
     content = f.readlines()
 # you may also want to remove whitespace characters like `\n` at the end of each line
 content = [x.strip() for x in content]
